2bd4d2faa2ac05e774426ba094ad7f5a5e125af1
turtlelazy <ishraqmahid@gmail.com>
2022-10-11 14:44:22 -0400

    test-ascii

---
+++ test-ascii.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main(){
+    std::string s;
+    std::getline(std::cin , s);
+    for(int i = 0; i < s.length();i++){
+        char c = s[i];
+        std::cout << c << " " << int(c) << std::endl;
+    }
+}
