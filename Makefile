4ace0657f1533e69bd698b199201ac96bf86273a
turtlelazy <ishraqmahid@gmail.com>
2022-10-14 01:49:46 -0400

    format

--- Makefile
+++ Makefile
@@ -13,4 +13,4 @@ main.o: main.cpp caesar.h vigenere.h decryption.h
 tests.o: tests.cpp doctest.h caesar.h vigenere.h decryption.h
 	g++ -c -std=c++11 tests.cpp
 clean:
-	rm -f main.o caesar.o tests.o vigenere.o decryption.o tests a.out
+	rm -f main.o caesar.o tests.o vigenere.o decryption.o tests a.out main
--- main.cpp
+++ main.cpp
@@ -6,20 +6,20 @@
 int main()
 {
     std::cout << "shiftChar 'a' by 5\n";
-    std::cout <<  shiftChar('a',5) << "\n";
+    std::cout <<  shiftChar('a',5) << "\n\n";
 
     std::cout << "Caeser Cipher Encryption: 'Hello World!' , Shift 5 \n";
-    std::cout << encryptCaesar("Hello World!",10) << "\n";
+    std::cout << encryptCaesar("Hello World!", 10) << "\n\n";
     std::string a = encryptCaesar("Hello World!",10);
 
     std::cout << "Vigenere Cipher Encryption: 'Hello World!' , Key 'cake' \n";
-    std::cout << encryptVigenere("Hello World!", "cake") << "\n";
+    std::cout << encryptVigenere("Hello World!", "cake") << "\n\n";
     std::string b = encryptVigenere("Hello World!", "cake");
 
     std::cout << "Decrypt Caesar: " << a << ", Shift 10\n";
-    std::cout << decryptCaesar(a,10) << "\n";
+    std::cout << decryptCaesar(a, 10) << "\n\n";
 
     std::cout << "Decrypt Vigenere: " << b << ", Key 'cake'\n";
-    std::cout << decryptVigenere(b,"cake") << "\n";
+    std::cout << decryptVigenere(b, "cake") << "\n\n";
     return 0;
 }
